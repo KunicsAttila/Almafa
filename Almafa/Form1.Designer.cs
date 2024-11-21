2c9e7889a5be0d97c58e50fe2f62a93c320293af
Kunics.Attila <Kunics.Attila@C19-02>
2024-11-21 20:42:15 +0100

    alma

--- Almafa/Form1.Designer.cs
+++ Almafa/Form1.Designer.cs
@@ -30,23 +30,31 @@ namespace Almafa
         private void InitializeComponent()
         {
             this.föld = new System.Windows.Forms.PictureBox();
-            this.fatorzs = new System.Windows.Forms.PictureBox();
-            this.pictureBox2 = new System.Windows.Forms.PictureBox();
-            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.torzs = new System.Windows.Forms.PictureBox();
+            this.lomb = new System.Windows.Forms.PictureBox();
+            this.kosar = new System.Windows.Forms.PictureBox();
             this.pictureBox4 = new System.Windows.Forms.PictureBox();
-            this.pictureBox5 = new System.Windows.Forms.PictureBox();
-            this.pictureBox6 = new System.Windows.Forms.PictureBox();
-            this.pictureBox7 = new System.Windows.Forms.PictureBox();
-            this.pictureBox8 = new System.Windows.Forms.PictureBox();
+            this.test = new System.Windows.Forms.PictureBox();
+            this.fej = new System.Windows.Forms.PictureBox();
+            this.kez = new System.Windows.Forms.PictureBox();
+            this.alma = new System.Windows.Forms.PictureBox();
+            this.appleCounter = new System.Windows.Forms.Label();
+            this.storageLabel = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.buyFasterHit = new System.Windows.Forms.Button();
+            this.BuyBiggerStorag = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.föld)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.fatorzs)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.torzs)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.lomb)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.kosar)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox8)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.test)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.fej)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.kez)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.alma)).BeginInit();
             this.SuspendLayout();
             // 
             // föld
@@ -58,32 +66,32 @@ namespace Almafa
             this.föld.TabIndex = 0;
             this.föld.TabStop = false;
             // 
-            // fatorzs
+            // torzs
             // 
-            this.fatorzs.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
-            this.fatorzs.Location = new System.Drawing.Point(107, 135);
-            this.fatorzs.Name = "fatorzs";
-            this.fatorzs.Size = new System.Drawing.Size(100, 256);
-            this.fatorzs.TabIndex = 1;
-            this.fatorzs.TabStop = false;
+            this.torzs.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.torzs.Location = new System.Drawing.Point(107, 135);
+            this.torzs.Name = "torzs";
+            this.torzs.Size = new System.Drawing.Size(100, 256);
+            this.torzs.TabIndex = 1;
+            this.torzs.TabStop = false;
             // 
-            // pictureBox2
+            // lomb
             // 
-            this.pictureBox2.BackColor = System.Drawing.Color.Green;
-            this.pictureBox2.Location = new System.Drawing.Point(23, 37);
-            this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(274, 101);
-            this.pictureBox2.TabIndex = 2;
-            this.pictureBox2.TabStop = false;
+            this.lomb.BackColor = System.Drawing.Color.Green;
+            this.lomb.Location = new System.Drawing.Point(23, 37);
+            this.lomb.Name = "lomb";
+            this.lomb.Size = new System.Drawing.Size(274, 101);
+            this.lomb.TabIndex = 2;
+            this.lomb.TabStop = false;
             // 
-            // pictureBox3
+            // kosar
             // 
-            this.pictureBox3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
-            this.pictureBox3.Location = new System.Drawing.Point(569, 325);
-            this.pictureBox3.Name = "pictureBox3";
-            this.pictureBox3.Size = new System.Drawing.Size(139, 66);
-            this.pictureBox3.TabIndex = 3;
-            this.pictureBox3.TabStop = false;
+            this.kosar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.kosar.Location = new System.Drawing.Point(569, 325);
+            this.kosar.Name = "kosar";
+            this.kosar.Size = new System.Drawing.Size(139, 66);
+            this.kosar.TabIndex = 3;
+            this.kosar.TabStop = false;
             // 
             // pictureBox4
             // 
@@ -94,81 +102,175 @@ namespace Almafa
             this.pictureBox4.TabIndex = 4;
             this.pictureBox4.TabStop = false;
             // 
-            // pictureBox5
+            // test
             // 
-            this.pictureBox5.Location = new System.Drawing.Point(422, 256);
-            this.pictureBox5.Name = "pictureBox5";
-            this.pictureBox5.Size = new System.Drawing.Size(59, 135);
-            this.pictureBox5.TabIndex = 5;
-            this.pictureBox5.TabStop = false;
+            this.test.BackColor = System.Drawing.Color.White;
+            this.test.Location = new System.Drawing.Point(422, 256);
+            this.test.Name = "test";
+            this.test.Size = new System.Drawing.Size(59, 135);
+            this.test.TabIndex = 5;
+            this.test.TabStop = false;
             // 
-            // pictureBox6
+            // fej
             // 
-            this.pictureBox6.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
-            this.pictureBox6.Location = new System.Drawing.Point(407, 195);
-            this.pictureBox6.Name = "pictureBox6";
-            this.pictureBox6.Size = new System.Drawing.Size(94, 66);
-            this.pictureBox6.TabIndex = 6;
-            this.pictureBox6.TabStop = false;
+            this.fej.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.fej.Location = new System.Drawing.Point(407, 195);
+            this.fej.Name = "fej";
+            this.fej.Size = new System.Drawing.Size(94, 66);
+            this.fej.TabIndex = 6;
+            this.fej.TabStop = false;
             // 
-            // pictureBox7
+            // kez
             // 
-            this.pictureBox7.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
-            this.pictureBox7.Location = new System.Drawing.Point(348, 288);
-            this.pictureBox7.Name = "pictureBox7";
-            this.pictureBox7.Size = new System.Drawing.Size(100, 41);
-            this.pictureBox7.TabIndex = 7;
-            this.pictureBox7.TabStop = false;
+            this.kez.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.kez.Location = new System.Drawing.Point(348, 288);
+            this.kez.Name = "kez";
+            this.kez.Size = new System.Drawing.Size(100, 41);
+            this.kez.TabIndex = 7;
+            this.kez.TabStop = false;
             // 
-            // pictureBox8
+            // alma
             // 
-            this.pictureBox8.BackColor = System.Drawing.Color.Red;
-            this.pictureBox8.Location = new System.Drawing.Point(236, 166);
-            this.pictureBox8.Name = "pictureBox8";
-            this.pictureBox8.Size = new System.Drawing.Size(33, 38);
-            this.pictureBox8.TabIndex = 8;
-            this.pictureBox8.TabStop = false;
+            this.alma.BackColor = System.Drawing.Color.Red;
+            this.alma.Location = new System.Drawing.Point(240, 135);
+            this.alma.Name = "alma";
+            this.alma.Size = new System.Drawing.Size(20, 20);
+            this.alma.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.alma.TabIndex = 8;
+            this.alma.TabStop = false;
+            // 
+            // appleCounter
+            // 
+            this.appleCounter.AutoSize = true;
+            this.appleCounter.Location = new System.Drawing.Point(619, 37);
+            this.appleCounter.Name = "appleCounter";
+            this.appleCounter.Size = new System.Drawing.Size(119, 13);
+            this.appleCounter.TabIndex = 9;
+            this.appleCounter.Text = "Gyűjtött almák száma: 0";
+            // 
+            // storageLabel
+            // 
+            this.storageLabel.AutoSize = true;
+            this.storageLabel.Location = new System.Drawing.Point(619, 62);
+            this.storageLabel.Name = "storageLabel";
+            this.storageLabel.Size = new System.Drawing.Size(134, 13);
+            this.storageLabel.TabIndex = 10;
+            this.storageLabel.Text = "Kosár teherbírása: 20 alma";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(527, 101);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(127, 13);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "Nagyobb kosár vásárlása";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(562, 114);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 13);
+            this.label2.TabIndex = 13;
+            this.label2.Text = "(+ 5 alma)";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(562, 148);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(48, 13);
+            this.label3.TabIndex = 16;
+            this.label3.Text = "(- 1 ütés)";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(527, 135);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(88, 13);
+            this.label4.TabIndex = 15;
+            this.label4.Text = "Gyorsabb szedés";
+            // 
+            // buyFasterHit
+            // 
+            this.buyFasterHit.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.buyFasterHit.Location = new System.Drawing.Point(672, 130);
+            this.buyFasterHit.Name = "buyFasterHit";
+            this.buyFasterHit.Size = new System.Drawing.Size(75, 23);
+            this.buyFasterHit.TabIndex = 14;
+            this.buyFasterHit.Text = "30 alma";
+            this.buyFasterHit.UseVisualStyleBackColor = false;
+            // 
+            // BuyBiggerStorag
+            // 
+            this.BuyBiggerStorag.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.BuyBiggerStorag.Location = new System.Drawing.Point(672, 96);
+            this.BuyBiggerStorag.Name = "BuyBiggerStorag";
+            this.BuyBiggerStorag.Size = new System.Drawing.Size(75, 23);
+            this.BuyBiggerStorag.TabIndex = 17;
+            this.BuyBiggerStorag.Text = "10 alma";
+            this.BuyBiggerStorag.UseVisualStyleBackColor = false;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.pictureBox8);
-            this.Controls.Add(this.pictureBox7);
-            this.Controls.Add(this.pictureBox6);
-            this.Controls.Add(this.pictureBox5);
+            this.Controls.Add(this.kosar);
+            this.Controls.Add(this.BuyBiggerStorag);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.buyFasterHit);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.storageLabel);
+            this.Controls.Add(this.appleCounter);
+            this.Controls.Add(this.alma);
+            this.Controls.Add(this.kez);
+            this.Controls.Add(this.fej);
+            this.Controls.Add(this.test);
             this.Controls.Add(this.pictureBox4);
-            this.Controls.Add(this.pictureBox3);
-            this.Controls.Add(this.pictureBox2);
-            this.Controls.Add(this.fatorzs);
+            this.Controls.Add(this.lomb);
+            this.Controls.Add(this.torzs);
             this.Controls.Add(this.föld);
             this.Name = "Form1";
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.föld)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.fatorzs)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.torzs)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.lomb)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.kosar)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox8)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.test)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.fej)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.kez)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.alma)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.PictureBox föld;
-        private System.Windows.Forms.PictureBox fatorzs;
-        private System.Windows.Forms.PictureBox pictureBox2;
-        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox torzs;
+        private System.Windows.Forms.PictureBox lomb;
+        private System.Windows.Forms.PictureBox kosar;
         private System.Windows.Forms.PictureBox pictureBox4;
-        private System.Windows.Forms.PictureBox pictureBox5;
-        private System.Windows.Forms.PictureBox pictureBox6;
-        private System.Windows.Forms.PictureBox pictureBox7;
-        private System.Windows.Forms.PictureBox pictureBox8;
+        private System.Windows.Forms.PictureBox test;
+        private System.Windows.Forms.PictureBox fej;
+        private System.Windows.Forms.PictureBox kez;
+        private System.Windows.Forms.PictureBox alma;
+        private System.Windows.Forms.Label appleCounter;
+        private System.Windows.Forms.Label storageLabel;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button buyFasterHit;
+        private System.Windows.Forms.Button BuyBiggerStorag;
     }
 }
 
--- Almafa/Form1.cs
+++ Almafa/Form1.cs
@@ -8,13 +8,208 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+
+/*Hozz létre egy fát és egy embert, az embernek nem kötlező hogy feje legyen
+Hozz létre egy kosarat
+Az ember a kosár és a fa törzse között sétál oda-vissza
+Amikor a fához ér, elkezdi azt a kezével ütni. Az ütés animálása kötelező
+Miután az ember 10-szer megütötte a fát, egy alma hulljon le a lombból, pont az ember kezébe. A kezénél álljon meg
+Amikor megvolt a 10. ütés, nem kell többet ütni, már csak a hulló almára vár az ember
+Amint elkapta az almát, visszafordul, ez lehet egy hirtelen fordulás, majd elindul almával a kezében vissza a kosárhoz
+Amikor elérte a kosarat, a kezében lévő alma folytatja lefelé hullását addig, amíg a kosár teljesen el nem nyeli
+Amikor a kosár elnyelte az almát, a gyűjtött almák száma eggyel nő
+  Ha a gyűjtött almák száma elérte a kosár teherbírását, több almát nem tárolunk el
+Amint leesett az alma és elnyelte a kosár, az ember visszafordul és újrakezdi útját
+
+A nagyobb kosár vásárlása gomb illetve a gyorsabb szedés gomb csak akkor működik, ha van elég almánk a fejlesztés megvásárlásához.
+   nagyobb kosár vásárlása esetén:
+     a kosár teherbírása 5 almával nő
+     a nagyobb kosár ára 2 almával nő
+   gyorsabb szedés vásárlása esetén:
+    az alma leszedéséhez 1-el kevesebb ütés szükséges, de minimum 3
+    a gyorsabb szedés ára 30 almával nő*/
+
+
 namespace Almafa
 {
     public partial class Form1 : Form
     {
+        Timer walkTimer = new Timer();
+        Timer hitTimer = new Timer();
+        Timer gravity = new Timer();
+
+        int hitCount = 0;
+        int hitDirection = 0; // 1: balra; 0: nyugalmi állapot; -1: jobbra
+        int hitFrames = 0;
+        int hitMaxFrames = 3;
+        int requiredHits = 10;
+        int apples = 0;
+        int storageCapacity = 20;
+        int priceOfBiggerStorage = 10;
+        int priceOfFasterHit = 30;
+
+        bool holdingAnApple = false;
+        bool facingLeft = true;
+
         public Form1()
         {
             InitializeComponent();
+            Start();
+        }
+        void Start()
+        {
+            StartTimers();
+            AddEvents();
+        }
+        void AddEvents()
+        {
+            BuyBiggerStorag.Click += BuyBiggerStorage;
+            buyFasterHit.Click += BuyFasterHit;
+        }
+        void StartTimers()
+        {
+            walkTimer.Interval = 16;
+            hitTimer.Interval = 16;
+            gravity.Interval = 16;
+            walkTimer.Start();
+            walkTimer.Tick += walkEvent;
+            hitTimer.Tick += hitEvent;
+            gravity.Tick += GravityEvent;
+        }
+        void walkEvent(object s, EventArgs e)
+        {
+            if (kez.Left > torzs.Right && !holdingAnApple)
+            {
+                fej.Left -= 3;
+                test.Left -= 3;
+                kez.Left -= 3;
+            }
+            else if (!holdingAnApple)
+            {
+                walkTimer.Stop();
+                hitDirection = 1;
+                hitTimer.Start();
+            }
+            else if (holdingAnApple && test.Right < kosar.Left)
+            {
+                fej.Left += 3;
+                test.Left += 3;
+                kez.Left += 3;
+                alma.Left += 3;
+            }
+            else if (holdingAnApple && test.Right >= kosar.Left)
+            {
+                walkTimer.Stop();
+                gravity.Start();
+            }            
+        }
+        void hitEvent(object s, EventArgs e)
+        {
+            if (hitDirection == 1)
+            {
+                kez.Left -= 6; // gyorsabb legyen, mint az ember mozgása (ami: 3 pixel)!
+                hitFrames++;
+                if (hitFrames == hitMaxFrames)
+                {
+                    hitDirection = -1;
+                    hitFrames = 0;
+                }
+            }
+            else if (hitDirection == -1)
+            {
+                kez.Left += 6; 
+                hitFrames++;
+                if (hitFrames == hitMaxFrames)
+                {
+                    hitFrames = 0;
+                    hitCount++;
+                    this.Text = hitCount.ToString();
+                    if (hitCount == requiredHits)
+                    {
+                        hitDirection = 0;
+                        hitTimer.Stop();
+                        alma.Left = kez.Left;
+                        alma.Top = lomb.Bottom - alma.Height;
+                        alma.Show(); // alma.Visible = True;
+                        gravity.Start();
+                    }
+                    else
+                    {
+                        hitDirection = 1;
+                    }
+                }
+            }
+        }
+        void GravityEvent(object s, EventArgs e)
+        {
+            if (alma.Bottom < kez.Top && !holdingAnApple)
+            {
+                alma.Top += 3;
+            }
+            else if (!holdingAnApple)
+            {
+                holdingAnApple = true;
+                gravity.Stop();
+                kez.Left = test.Left + test.Width / 2;
+                alma.Left = kez.Right - alma.Width;
+                walkTimer.Start();
+            }
+            else if (holdingAnApple)
+            {
+                alma.Top += 3;
+                if (alma.Top > kosar.Top)
+                {
+                    gravity.Stop();
+                    holdingAnApple = false; // Itt már nem tartjuk az almát!
+                    kez.Left = test.Left + test.Width / 2 - kez.Width;
+                    hitCount = 0;
+                    if (apples < storageCapacity)
+                    {
+                        apples++;
+                    }
+                    UpdateAppleLabel();
+
+                    walkTimer.Start();
+                }
+            }
+        }
+
+        void UpdateAppleLabel()
+        {
+            appleCounter.Text = $"Gyűjtött almák száma: {apples}";
+        }
+        //Kosár teherbírása: 20 alma
+        void UpdateStorageLabel()
+        {
+            storageLabel.Text = $"Kosár teherbírása: {storageCapacity} alma";
+        }
+
+        void BuyBiggerStorage(object sender, EventArgs e)
+        {
+            if (apples >= priceOfBiggerStorage)
+            {
+                apples -= priceOfBiggerStorage;
+                storageCapacity += 5;
+                priceOfBiggerStorage += 2;
+                BuyBiggerStorag.Text = $"{priceOfBiggerStorage} alma";
+
+                UpdateAppleLabel();
+                UpdateStorageLabel();
+            }
+        }
+        void BuyFasterHit(object sender, EventArgs e)
+        {
+            if (apples >= priceOfFasterHit && requiredHits > 3)
+            {
+                apples -= priceOfFasterHit;
+                requiredHits--;
+                priceOfFasterHit += 30;
+                buyFasterHit.Text = $"{priceOfFasterHit} alma";
+
+                UpdateAppleLabel();
+            }
         }
     }
 }
+
+// Eltünik az alma a kosárban: Form1.cs[Design] / Kosár/ jobb gomb/ Bring to front
